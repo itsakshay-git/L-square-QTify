2409fc9decfc30558f61aee427eb6e7dfe55d96b
Akshay <ak.dhongade01@gmail.com>
2023-03-05 20:04:14 +0530

    C2 Navbar Update

--- my-app/src/components/SearchBar/SearchBar.jsx
+++ my-app/src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import style from "./Search.module.css"
 
 const SearchBar = () => {
   return (
     <>
-      <div>
-        <input type="search" value="search a song of your choice" />
-        <button type="submit">
+      <div className={style.wrapper}>
+        <input className={style.searchbar} type="search" placeholder="search a song of your choice" />
+        <button type="submit" className={style.buttonIcon}>
             <img src={require("../../assets/search.png")} alt=""  width="25px"/>
         </button>
       </div>
